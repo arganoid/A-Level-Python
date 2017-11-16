6d9438fb26fd0c94117682282570db609343897a
Andrew Gillett <arganoid@pair.com>
2017-11-16 11:32:37 +0000

    - Rename ClassInheritance to animals_oop - Demonstrate static/class variables, add breed to Dog

--- OOP/animals_oop.py
+++ OOP/animals_oop.py
@@ -1,8 +1,12 @@
 class Animal:
 
+    num_animals = 0
+
     def __init__(self, name, age):
         self.age = age
         self.name = name
+        Animal.num_animals += 1
+        print(Animal.num_animals)
 
     def make_noise(self):
         print("?")
@@ -13,8 +17,9 @@ class Animal:
 
 class Dog(Animal):
 
-    def __init__(self, name, age):
+    def __init__(self, name, age, breed):
         super().__init__(name, age)
+        self.breed = breed
 
     def make_noise(self):
         print("woof")
@@ -29,10 +34,10 @@ class Cat(Animal):
         print("meow")
 
 
-my_dog = Dog("Fido", 3)
+my_dog = Dog("Fido", 3, "Terrier")     # Instantiate (ie create) a dog object, store a reference to it in my_dog
 my_cat = Cat("Felix", 5)
 
-if False:
+if True:
 
     print(my_dog.name)
     my_dog.make_noise()
@@ -47,7 +52,7 @@ if False:
 
 
 animals = [my_dog, my_cat]
-animals.append( Dog("Rover", 1) )
+animals.append( Dog("Rover", 1, "Alsatian") )
 
 for animal in animals:
     animal.make_noise()
