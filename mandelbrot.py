ae30b9d87f6cbab5c0c2fb871b017e14572dd682
Andrew Gillett <arganoid@pair.com>
2018-04-02 16:16:33 +0100

    Add mandelbrot.py

---
+++ mandelbrot.py
@@ -0,0 +1,105 @@
+# This has nothing to do with the A-level syllabus (except perhaps for recursion) but is cool so I included it anyway
+# Requires pygame library
+
+# http://www.pygame.org/docs/ref/gfxdraw.html
+
+# The Mandelbrot set is the set of complex numbers c for which the function f c ( z ) = z^2 + c
+# does not diverge when iterated from z = 0, i.e., for which the sequence f c ( 0 ),  f c ( f c ( 0 ) ), etc.,
+# remains bounded in absolute value.
+
+import sys
+
+import pygame
+import pygame.gfxdraw
+
+max_iterations = 60
+threshold = 1000
+
+def f(z, c, iterations=0):
+    result = z*z + c
+    if abs(result) > threshold:
+        return iterations
+    else:
+        if iterations == max_iterations:
+            return 0
+        else:
+            return f(result, c, iterations+1)
+
+
+BLACK    = (   0,   0,   0)
+WHITE    = ( 255, 255, 255)
+GREEN    = (   0, 255,   0)
+RED      = ( 255,   0,   0)
+BLUE     = (   0,   0, 255)
+
+screen_size = (1200,700)
+screen = pygame.display.set_mode(screen_size)
+
+
+pygame.init()
+
+font = pygame.font.Font(None, 30)
+
+screen.fill(WHITE)
+scale = 0.3
+offsetX = -2.5
+offsetY = -1.0
+aspect = screen_size[0] / screen_size[1]
+
+cols = []
+for i in range(0,max_iterations+1):
+    c = (i / max_iterations) * 255
+    cols.append( (c,c,c) )
+
+while True:
+    screen.fill(WHITE)
+    for py in range(0, screen_size[1]):
+        for px in range(0, screen_size[0]):
+            for event in pygame.event.get():  # User did something
+                if event.type == pygame.QUIT:  # If user clicked close
+                    sys.exit()
+            x = (px / screen_size[0] / scale) + offsetX
+            y = (py / screen_size[1] / scale / aspect) + offsetY
+            result = f(0, complex(x, y))
+            pygame.gfxdraw.pixel(screen, px, py, cols[result])
+            if px == 0:
+                pygame.display.flip()
+
+    pygame.display.flip()
+
+    kp = False
+    while not kp:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: # If user clicked close
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_MINUS:
+                    scale *= 0.8
+                    kp = True
+                elif event.key == pygame.K_EQUALS:
+                    scale *= 1.2
+                    kp = True
+                elif event.key == pygame.K_LEFT:
+                    offsetX -= 0.1 / scale
+                    kp = True
+                elif event.key == pygame.K_RIGHT:
+                    offsetX += 0.1 / scale
+                    kp = True
+                elif event.key == pygame.K_UP:
+                    offsetY -= 0.1 / scale
+                    kp = True
+                elif event.key == pygame.K_DOWN:
+                    offsetY += 0.1 / scale
+                    kp = True
+                elif event.key == pygame.K_1:
+                    max_iterations -= 1
+                    kp = True
+                elif event.key == pygame.K_2:
+                    max_iterations += 1
+                    kp = True
+                elif event.key == pygame.K_3:
+                    threshold *= 0.75
+                    kp = True
+                elif event.key == pygame.K_4:
+                    threshold *= 1.25
+                    kp = True
