ba9e6da451d190758322f100c636aa4225ced2ee
Andrew Gillett <arganoid@pair.com>
2018-07-28 20:52:46 +0100

    Fix convex combinations.py

--- vectors/convex combinations.py
+++ vectors/convex combinations.py
@@ -1,10 +1,6 @@
 import random
 import sys
-import time
-
 import pygame
-
-# 0.4  0.1 0.1 0.4
 
 def make_random_convex_combination(polygon):
     # Create random number 0 to 1 for each point's x and y
@@ -17,9 +13,7 @@ def make_random_convex_combination(polygon):
 
     return make_convex_combination(polygon,combination_factors, total)
 
-
 def make_convex_combination(polygon, combination_factors, total=1):
-
     combination_x = 0
     combination_y = 0
     for i in range(len(polygon)):
@@ -31,7 +25,6 @@ def make_convex_combination(polygon, combination_factors, total=1):
 
 pygame.init()
 
-# Define some colors
 BLACK    = (   0,   0,   0)
 WHITE    = ( 255, 255, 255)
 GREEN    = (   0, 255,   0)
@@ -41,17 +34,13 @@ BLUE     = (   0,   0, 255)
 screen_size = (800,600)
 screen = pygame.display.set_mode(screen_size)
 
-font = pygame.font.Font(None, 50)
-
-# Used to manage how fast the screen updates
-clock = pygame.time.Clock()
-
 #polygon = [ [400,50], [700,100], [750,300], [400,500], [100,300], [200,200]]
 #polygon = [ [100,100], [500,100], [700,300], [500,500], [100,500]]
 polygon = [ [400,50], [700,500], [100,500]]
 
 screen.fill(WHITE)
 
+# Draw polygon
 for i in range(0, len(polygon)):
     pos1 = polygon[i]
     pos2 = polygon[(i + 1) % len(polygon)]
@@ -62,33 +51,8 @@ while True:
         if event.type == pygame.QUIT: # If user clicked close
             sys.exit()
 
-    combination_factors = [0,0,0]
-    for i1 in range(1,101):
-        combination_factors[0] = i1 * 0.01
-        for i2 in range(1,101):
-            combination_factors[1] = i2 * 0.01
-            for i3 in range(1, 101):
-                combination_factors[2] = i2 * 0.01
-                total = sum(combination_factors)
-                for i in range(len(combination_factors)):
-                    combination_factors[i] /= total
-                combination_pos = make_convex_combination(polygon,combination_factors, total)
+    combination_pos = make_random_convex_combination(polygon)
+    colour = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    pygame.draw.circle(screen, colour, combination_pos, 3)
 
-                r = random.randint(0, 200)
-                g = random.randint(0, 200)
-                b = random.randint(0, 200)
-                pygame.draw.circle(screen, (r, g, b), combination_pos, 3)
-
-                pygame.display.flip()
-
-    #combination_pos = make_random_convex_combination(polygon)
-    # r = random.randint(0, 200)
-    # g = random.randint(0, 200)
-    # b = random.randint(0, 200)
-    # pygame.draw.circle(screen, (r, g, b), combination_pos, 3)
-    #
-    # pygame.display.flip()
-
-
-
-
+    pygame.display.flip()
