2abbc23e06e86706c005b88fedc2d75bcf0ebbdb
Andrew Gillett <arganoid@pair.com>
2018-08-08 10:00:26 +0100

    Add Voronoi diagram generator

---
+++ coolstuff/voronoi.py
@@ -0,0 +1,82 @@
+# This has nothing to do with the A-level syllabus but is cool so I included it anyway
+# Requires pygame library
+
+# http://www.pygame.org/docs/ref/gfxdraw.html
+
+import sys
+import random
+import functools
+import profiler
+
+import pygame
+import pygame.gfxdraw
+
+BLACK    = (   0,   0,   0)
+WHITE    = ( 255, 255, 255)
+GREEN    = (   0, 255,   0)
+RED      = ( 255,   0,   0)
+BLUE     = (   0,   0, 255)
+
+pygame.init()
+
+screen_size = (1280,720)
+screen = pygame.display.set_mode(screen_size)
+
+screen.fill(WHITE)
+
+num_points = 100
+
+points = []
+for i in range(0,num_points):
+    pos = [ random.randint(0,screen_size[0]), random.randint(0,screen_size[1]) ]
+    col = ( random.randint(0,255), random.randint(0,255), random.randint(0,255) )
+    vel = ( random.randrange(-1,1), random.randrange(-1,1) )
+    points.append( (pos, col, vel) )
+
+image_idx = 0
+
+line_profiler = profiler.Profiler()
+frame_profiler = profiler.Profiler()
+
+while True:
+    for py in range(0, screen_size[1]):
+        for px in range(0, screen_size[0]):
+            for event in pygame.event.get():  # User did something
+                if event.type == pygame.QUIT:  # If user clicked close
+                    sys.exit()
+
+            # timings: 720p, 1000 points
+
+            # 1.6s per line
+            # nearest_point = sorted(points, key=lambda p: ((px-p[0][0]) ** 2 + (py-p[0][1]) ** 2))[0]
+
+            # 2.6s per line
+            # def closer(p1,p2):
+            #     return p1 if ((px - p1[0][0]) ** 2 + (py - p1[0][1]) ** 2) < ((px - p2[0][0]) ** 2 + (py - p2[0][1]) ** 2) else p2
+            # nearest_point = functools.reduce( closer, points, ((1e9,1e9),) )
+
+            # 1.45s per line
+            best_dist = 1e9
+            for point in points:
+                dist = (px-point[0][0]) ** 2 + (py-point[0][1]) ** 2
+                if dist < best_dist:
+                    nearest_point = point
+                    best_dist = dist
+
+            pygame.gfxdraw.pixel(screen, px, py, nearest_point[1])
+
+            if px == 0:
+                print("Line: " + str(line_profiler.get_seconds()))
+                pygame.display.flip()
+                line_profiler = profiler.Profiler()
+
+    print("Frame: " + frame_profiler.get_seconds())
+    pygame.display.flip()
+    frame_profiler = profiler.Profiler()
+
+#    pygame.image.save(screen, f"img{image_idx:04}.png" )
+#    image_idx += 1
+
+    for point in points:
+        point[0][0] += point[2][0]
+        point[0][1] += point[2][1]
