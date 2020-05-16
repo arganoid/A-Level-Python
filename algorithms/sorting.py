edfc2e06e305240ffdc73e9b390db4998ecd71d2
Andrew Gillett <arganoid@pair.com>
2020-05-16 20:48:30 +0100

    sorting.py: quick sort improvements; rename list params to l

--- algorithms/sorting.py
+++ algorithms/sorting.py
@@ -1,42 +1,42 @@
 # Bubble sort is simple but very slow compared to other sorting algorithms
-def bubble_sort(list, print_func, swap_animation = None):
-    print_func(list, 0)
+def bubble_sort(l, print_func, swap_animation = None):
+    print_func(l, 0)
     steps = 0
-    for i in range(0,len(list)-1):
-        for j in range(0, len(list)-1):
+    for i in range(0, len(l) - 1):
+        for j in range(0, len(l) - 1):
             steps = steps + 1
-            if list[j] > list[j+1]:
+            if l[j] > l[j + 1]:
                 if swap_animation != None:
-                    swap_animation(list, j, j+1)
+                    swap_animation(l, j, j + 1)
 
                 # Swap items using a temporary variable
                 # See bubble_sort_optimised below for an alternative way of doing this in Python
-                temp = list[j+1]
-                list[j+1] = list[j]
-                list[j] = temp
-            print_func(list, j)
+                temp = l[j + 1]
+                l[j + 1] = l[j]
+                l[j] = temp
+            print_func(l, j)
 
     print("Sorted in " + str(steps) + " steps")
 
 # Bubble sort is simple but very slow compared to other sorting algorithms
-def bubble_sort_optimised(list, print_func, swap_animation = None):
-    print_func(list, 0)
+def bubble_sort_optimised(l, print_func, swap_animation = None):
+    print_func(l, 0)
     steps = 0
     anySwapsMade = True
     while anySwapsMade:
         anySwapsMade = False
-        for i in range(0, len(list)-1):
+        for i in range(0, len(l) - 1):
             steps = steps + 1
-            if list[i] > list[i+1]:
+            if l[i] > l[i + 1]:
                 if swap_animation != None:
-                    swap_animation(list, i, i+1)
+                    swap_animation(l, i, i + 1)
 
                 # Swap elements - Python allows this kind of double assignment which removes the need for a temporary variable
                 # Don't do this in pseudocode!
-                list[i + 1], list[i] = list[i], list[i + 1]
+                l[i + 1], l[i] = l[i], l[i + 1]
 
                 anySwapsMade = True
-            print_func(list, i)
+            print_func(l, i)
 
     print("Sorted in " + str(steps) + " steps")
 
@@ -81,72 +81,81 @@ def merge_sort(data_list, start, end, print_func):
 # Insertion sort is faster than bubble sort (although still very
 # poor for large arrays) and uses less memory than merge sort.
 # Not required for AQA A-Level.
-def insertion_sort(list, print_func, swap_animation = None):
-    print_func(list, 0)
-    for i in range(1,len(list)):
+def insertion_sort(l, print_func, swap_animation = None):
+    print_func(l, 0)
+    for i in range(1, len(l)):
         j = i
-        print_func(list, j)
-        while j > 0 and list[j-1] > list[j]:
+        print_func(l, j)
+        while j > 0 and l[j - 1] > l[j]:
             # Shift current item down until it reaches its correct position
-            print_func(list, j)
+            print_func(l, j)
             if swap_animation != None:
-                swap_animation(list, j, j - 1)
+                swap_animation(l, j, j - 1)
 
             # Swap elements - Python allows this kind of double assignment which removes the need for a temporary variable
-            list[j-1], list[j] = list[j], list[j-1]
+            l[j - 1], l[j] = l[j], l[j - 1]
 
-            print_func(list, j)
+            print_func(l, j)
             j -= 1
 
 # This version is slightly faster as it reduces the number of assignments.
 # No swap_animation parameter as won't work with visualisation as it currently stands
-def insertion_sort_optimised(list, print_func):
-    print_func(list, 0)
-    for i in range(1,len(list)):
+def insertion_sort_optimised(l, print_func):
+    print_func(l, 0)
+    for i in range(1, len(l)):
         j = i
-        if list[j-1] > list[j]:
-            current = list[j]
-            while j > 0 and list[j-1] > current:
+        if l[j - 1] > l[j]:
+            current = l[j]
+            while j > 0 and l[j - 1] > current:
                 # Shift current item down until it reaches its correct position
-                print_func(list, i)
-                list[j] = list[j - 1]
+                print_func(l, i)
+                l[j] = l[j - 1]
                 j -= 1
-            list[j] = current
-
+            l[j] = current
 
 # Not required for AQA A-level
-def quick_sort(list, start, end, print_func, swap_animation = None):
+def quick_sort(l, start, end, print_func, swap_animation = None):
     if start < end:
-        p = quick_sort_partition(list, start, end, swap_animation)
-        print_func(list, p)
-        quick_sort(list, start, p, print_func)
-        quick_sort(list, p + 1, end, print_func)
+        pivot_index = quick_sort_partition(l, start, end, swap_animation)
+        print_func(l, pivot_index)
+        quick_sort(l, start, pivot_index, print_func)
+        quick_sort(l, pivot_index + 1, end, print_func)
 
-def quick_sort_partition(list, start, end, swap_animation = None):
-    pivot = list[start]
-    i = start - 1
-    j = end + 1
+def quick_sort_partition(l, start, end, swap_animation = None):
+    # Sort items in the given range to be either on the left or right side of a chosen pivot value. The index of
+    # the pivot value may move during this function. The final index of the pivot is returned.
+    # Choose middle element as partition. Any index can be chosen for the pivot but choosing the left-most index
+    # leads to worst-case performance on already-sorted lists. For large lists, choosing the median of the start, pivot
+    # and end values gives the best performance.
+    pivot_value = l[(end + start) // 2]
+
+    left = start
+    right = end
+
     while True:
-        i += 1
-        while list[i] < pivot:
-            i += 1
+        while l[left] < pivot_value:
+            left += 1
 
-        j -= 1
-        while list[j] > pivot:
-            j -= 1
+        while l[right] > pivot_value:
+            right -= 1
 
-        if i >= j:
-            return j
+        if left >= right:
+            # Lower/higher values have now been partitioned either side of the pivot (which may itself have moved)
+            return right
 
         if swap_animation != None:
-            swap_animation(list, i, j)
-        temp = list[i]
-        list[i] = list[j]
-        list[j] = temp
+            swap_animation(l, left, right)
+
+        # Python swap shorthand, most other languages require a temporary variable
+        l[left], l[right] = l[right], l[left]
+
+        left += 1
+        right -= 1
+
 
 if __name__ == "__main__":
-    #my_list = [5,1,2,9,8,1,4,3,5,0,7,3,1]
-    my_list = [5,1,2,9]
+    my_list = [5,1,2,9,8,1,4,3,5,0,7,3,1]
+    #my_list = [5,1,2,9]
 
     #bubble_sort(my_list, print)
     #merge_sort(my_list,0,len(my_list)-1, print)
