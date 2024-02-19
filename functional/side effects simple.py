30d65b985625317bfb4280606887c7abd61e02d5
Andrew Gillett <arg@arganoid.com>
2024-02-19 16:43:42 +0000

    Update side effects simple to show case where just calling the function twice gives different results

--- functional/side effects simple.py
+++ functional/side effects simple.py
@@ -1,12 +1,12 @@
 global_variable = 0
 
 def do_calculation(x):
+    global global_variable
+    global_variable += 1
     return x * 3 + global_variable
 
-print(do_calculation(10))   # returns 30
+print(do_calculation(10))   # returns 31
 
-global_variable = 5
-
-print(do_calculation(10))   # returns 35 - same function call with same parameter gave a different result.
+print(do_calculation(10))   # returns 32 - same function call with same parameter gave a different result.
                             # If we were working under the functional programming paradigm, this kind of thing
                             # should not be able to happen.
