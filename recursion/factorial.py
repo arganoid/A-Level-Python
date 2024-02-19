a8c4c4aa92732e7772fb49f03b5ead4761cd7c44
Andrew Gillett <arg@arganoid.com>
2024-02-19 16:47:42 +0000

    Update factorial.py with more pythonic names and improved comments

--- recursion/factorial.py
+++ recursion/factorial.py
@@ -2,8 +2,13 @@
 # Need help understanding computer science and programming?
 # Arganoid Tuition - https://tutor.arganoid.com/
 
+import sys
+
+# Allow very large results, in case you want to try calculating factorials of very large numbers!
+sys.set_int_max_str_digits(6000000)
+
 # Calculate factorial (eg 5! = 5 * 4 * 3 * 2 * 1) using a loop
-def factorialIterative(x):
+def factorial_iterative(x):
     result = 1
     for i in range(2, x+1):     # 2nd range parameter not inclusive
         result *= i
@@ -11,20 +16,21 @@ def factorialIterative(x):
 
 
 # Here's another way of thinking of how to calculate factorials:
-# 5!
-# 5 * 4!
-# 4 * 3!
-# 3 * 2!
-# 2 * 1!
+# 5! = 5 * 4!
+# 4! = 4 * 3!
+# 3! = 3 * 2!
+# 2! = 2 * 1!
 # 1! = 1
 
 # The following function qualifies as both recursive (because it calls itself)
 # and functional (as there are no side effects)
-def factorialRecursive(x):
+def factorial_recursive(x):
     if x <= 1:
+        # base case
         return x
     else:
-        return x * factorialRecursive(x-1)
+        return x * factorial_recursive(x - 1)
 
-print(factorialIterative(5))
-print(factorialRecursive(5))
+print(factorial_iterative(5))
+print(factorial_recursive(5))
+
